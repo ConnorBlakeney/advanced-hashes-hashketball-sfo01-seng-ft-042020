510462509953bc0f84a860f636d223228290b8fe
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-25 16:50:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,15 @@ def game_hash
       colors: 
       players: [
         {
-          
+          player_name: 'Jeff Adrien',
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slam_dunks: 2 
         }]
     }
     
